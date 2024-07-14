d6799576374c23d5d1b0e7b8f4f4b24edee7cf75
AQSA-ABDULLAH <aqsaabdullah38403gmail.com>
2024-07-15 03:29:14 +0500

    create statics

--- user/src/app/page.tsx
+++ user/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/sections/hero/hero";
+import Statics from "@/sections/statics/statics";
 
 export default function Home() {
   return (
     <>
       <Hero/>
+      <Statics/>
     </>
   );
 }
---
+++ user/src/sections/statics/statics.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Statics = () => {
+  return (
+    <div className="bg-white m-8 mx-40 rounded px-8 py-6 flex justify-between">
+      <div className="text-center">
+        <h2 className="text-3xl">70%</h2>
+        <p className="text-sm">Job Achievements</p>
+      </div>
+
+      <div className="text-center">
+        <h2 className="text-3xl">$8.4M</h2>
+        <p className="text-sm">Job Achievements</p>
+      </div>
+
+      <div className="text-center">
+        <h2 className="text-3xl">1.6K</h2>
+        <p className="text-sm">Job Achievements</p>
+      </div>
+
+      <div className="text-center">
+        <h2 className="text-3xl">+24</h2>
+        <p className="text-sm">Job Achievements</p>
+      </div>
+    </div>
+  );
+};
+
+export default Statics;
+
